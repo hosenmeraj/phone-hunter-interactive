753b2f17af75b64fc90bd97693faef34914d90b6
Merajhosen <merajakhon7@gmail.com>
2022-03-01 22:27:56 +0000

    getting api with fetch and show console log in a window

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('check');
+const loadPhone = () => {
+    const error = document.getElementById('error');
+    const searchFeild = document.getElementById('input-value')
+    const searchText = searchFeild.value;
+    if (searchText == '') {
+        error.innerText = 'Please, write a product name';
+        searchFeild.value = '';
+    } else {
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+            .then(response => response.json())
+            .then(data => console.log(data.data))
+    }
+
+}
