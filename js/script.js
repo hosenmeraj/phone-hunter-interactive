7d074be0ca02eaa27763eeb38ef1e1f6010951d8
Merajhosen <merajakhon7@gmail.com>
2022-03-02 18:42:36 +0000

    spinner add

--- js/script.js
+++ js/script.js
@@ -1,28 +1,42 @@
 const error = document.getElementById('error');
+const searchPhone = document.getElementById('search-phones')
+const phoneDetails = document.getElementById('phone-details')
+// spinner
+const spinner = displayLoading => {
+    document.getElementById('spinner').style.display = displayLoading;
+}
 const loadPhone = () => {
-
     const searchFeild = document.getElementById('input-value')
     const searchText = searchFeild.value;
+
     if (searchText === '') {
         error.innerText = 'Please, write a product name';
         searchFeild.value = '';
+        spinner("none")
+        searchPhone.textContent = '';
 
     } else if (isNaN(searchText) === false) {
         error.innerText = 'Product name can not be a Number!';
         searchFeild.value = '';
+        spinner("none")
+        searchPhone.textContent = '';
     } else {
         fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
             .then(response => response.json())
             .then(data => displayPhone(data.data))
+        spinner("block")
+        error.textContent = '';
+        searchFeild.value = '';
     }
     searchFeild.value = '';
+
 
 }
 const displayPhone = phones => {
     if (phones.length === 0) {
         error.innerText = 'No Product Found!'
+        spinner("none")
     } else {
-        const searchPhone = document.getElementById('search-phones')
         searchPhone.textContent = '';
         const twentyElement = phones.slice(0, 20)
         for (const phone of twentyElement) {
@@ -30,8 +44,10 @@ const displayPhone = phones => {
             const div = document.createElement('div')
             div.classList.add('col')
             div.innerHTML = `
-        <div class="card h-100">
-        <img src="${phone.image}" class="card-img-top img-fluid rounded" alt="...">
+        <div class="card h-100 rounded shadow"">
+        <div class="d-flex justify-content-center p-2">
+        <img src="${phone.image}" class="card-img-top img-fluid w-75 rounded" alt="...">
+        </div>
         <div class="card-body text-center">
             <h5 class="card-title">${phone.phone_name}</h5>
             <p class="card-text">${phone.brand}</p>
@@ -42,17 +58,24 @@ const displayPhone = phones => {
         </div>
         `
             searchPhone.appendChild(div)
+            spinner("none")
         }
+        phoneDetails.textContent = '';
     }
 }
+
+
 const loadPhoneDetails = (phonId) => {
     const url = `https://openapi.programming-hero.com/api/phone/${phonId}`
     fetch(url)
         .then(response => response.json())
         .then(data => displayPhoneDetails(data.data))
+    document.getElementById('phone-details').textContent = '';
+
 }
 const displayPhoneDetails = (detail) => {
     // console.log(detail);
+    phoneDetails.textContent = '';
     const connection = detail.others
     // 
     const other = []
@@ -63,17 +86,17 @@ const displayPhoneDetails = (detail) => {
     }
     const sensors = detail.mainFeatures.sensors;
     console.log(sensors);
-    const phoneDetails = document.getElementById('phone-details')
-    phoneDetails.textContent = '';
+
     const div = document.createElement('div')
-    div.classList.add('row', 'p-3')
+    div.classList.add('row', 'w-100')
     div.innerHTML = `
-        <div class="col-md-4 d-flex justy-content-center p-3">
-        <img src="${detail.image}" class="card-img-top" alt="...">
+        <div class="col-md-4 d-flex justy-content-center">
+        <img src="${detail.image}" class="card-img-top p-4" alt="...">
         </div>
         <div class="col-md-4">
         <div class="card-body">
             <h5 class="card-title fw-bold mb-3">${detail.name}</h5>
+            <h5 class="card-title">${detail.brand}</h5>
             <p class="card-text"><b>Release: </b>${detail.releaseDate ? detail.releaseDate : "No release date found!" }</p>
             <p class="card-text"><b>Chipset: </b>${detail.mainFeatures.chipSet }</p>
             <p class="card-text"><b>Memory: </b>${detail.mainFeatures.memory }</p>
@@ -95,10 +118,9 @@ const displayPhoneDetails = (detail) => {
                
         </div>
     </div>
-     
+    
     `
     phoneDetails.appendChild(div)
-
 
 
 }
